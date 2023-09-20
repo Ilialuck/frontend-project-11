6a0415fe44651722db8b8526f0c1a621ca2b92fc
Ilialuck <ilialuck@yandex.ru>
2023-09-20 22:43:55 +0300

    first

--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,29 @@ const config = {
     ],
     module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
             },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+          },
+          { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+          {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+          },
+          {
+            test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            use: 'url-loader?limit=10000',
+          },
+          {
+            test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+            use: 'file-loader',
+          },
         ],
     },
 };
@@ -50,3 +62,15 @@ module.exports = () => {
     }
     return config;
 };
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
+
